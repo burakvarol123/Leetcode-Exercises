52a5b35d2cded72727ef0ff130169163284c71c2
Burak Varol <124465068+burakvarol123@users.noreply.github.com>
2026-01-13 23:50:15 +0000

    Time: 3 ms (77.39%) | Memory: 12.6 MB (35.19%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        mapping = { ")":"(", "}": "{", "]" : "["}
+        for char in s:
+            if char in mapping:
+                top = stack.pop() if stack else "#"
+                if top != mapping[char]:
+                    return False
+            else:
+                stack.append(char)
+
+        return not stack
