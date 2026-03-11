e479971fcf376b25905cbba491bb8a8f9e4e729e
Burak Varol <124465068+burakvarol123@users.noreply.github.com>
2026-03-11 15:06:55 +0100

    Time: 3 ms (67.06%) | Memory: 19.3 MB (56.28%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,18 +1,12 @@
-class Solution(object):
-    def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        if x < 2:
-            return x
-        left, right = 1, x
-        while left<=right:
-            mid = (left + right) // 2
-            if mid * mid == x:
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right = 0, x
+        while left <= right:
+            mid = (left +right) //2
+            if mid*mid == x:
                 return mid
-            elif mid * mid < x:
-                left = mid + 1
-            else:
-                right = mid-1
+            elif mid*mid < x:
+                left = mid +1
+            else: 
+                right = mid - 1
         return right
